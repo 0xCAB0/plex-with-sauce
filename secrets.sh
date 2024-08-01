fd42382eccb8b843185cdb3835d371f0925c7085
0xCAB0 <alvarocaboac2@gmail.com>
2024-08-01 17:26:11 +0200

    Removed .env and added README

---
+++ secrets.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#
+detect-secrets scan --all-files --disable-plugin AbsolutePathDetectorExperimental --exclude-files $SECRETS_REGEX --exclude-files '\.git*' --exclude-files '.env-py' > $SECRETS_OUTFILE && detect-secrets audit $SECRETS_OUTFILE
